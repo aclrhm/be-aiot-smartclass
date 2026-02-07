1554fee33d85d222a8a6047e8b89d99ee7fa5b59
Azka Auliarahma <108479171+aclrhm@users.noreply.github.com>
2026-02-07 13:18:50 +0700

    lagi

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("../routes/index"));
+app.use("/api", require("./routes/index"));
 
 app.get("/", (req, res) => {
   res.send("Backend AIoT aktif 🚀");
