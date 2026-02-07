39ab580251b455515d5eb326ed45aa8743ea0fae
Azka Auliarahma <108479171+aclrhm@users.noreply.github.com>
2026-02-07 13:02:20 +0700

    adalah pokonya

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,14 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json'); 
+const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, "\n")
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const db = admin.firestore();
 const auth = admin.auth();
