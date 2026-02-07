8eb739a0e9e40e78b7f7cc0e5dee0fe5e1474b7e
Azka Auliarahma <108479171+aclrhm@users.noreply.github.com>
2026-02-07 13:16:53 +0700

    benerin api lagi

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', require('./routes/index'));
+app.use("/api", require("../routes/index"));
 
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Backend AIoT aktif di http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Backend AIoT aktif 🚀");
 });
+
+module.exports = app; 
