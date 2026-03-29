32263c20865a6c594292669f58c354967e1195c6
Azka Auliarahma <azkaauliarahma08@gmail.com>
2026-03-29 12:17:59 +0700

    login

--- config/firebase.js
+++ config/firebase.js
@@ -10,8 +10,8 @@ if (!admin.apps.length) {
   });
 }
 
-
+const WEB_API_KEY = "AIzaSyA3flNDN9l1y_ou-eVU0wuo8NHouhgL_po";
 const db = admin.firestore();
 const auth = admin.auth();
 
-module.exports = { admin, db, auth };
+module.exports = { admin, db, auth, WEB_API_KEY };
